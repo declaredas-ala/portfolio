bd00bbe69f15e257440eeb7e5bf8747c3ff7ffa0
declaredas-ala <alamissaoui.dev@gmail.com>
2023-09-07 17:13:30 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import Home from './components/Home'
+
+import About from './components/About'
+import Skills from './components/Skills'
+import Work from './components/work'
+import Contact from './components/Contact'
+
+export default function App() {
+  return (
+    <>
+    <Navbar/>
+    <Home/>
+    <About/>
+    <Skills/>
+    <Work/>
+    <Contact/>
+    
+    
+    
+    </>
+  )
+}
---
+++ src/data/data.js
@@ -0,0 +1,36 @@
+import WorkIm from '../assets/projects/workImg.jpeg'
+import RealEst from '../assets/projects/realestate.jpg'
+import project from '../assets/projects/project.png'
+
+export const data=[
+    {
+        id:1,
+        name:"Fitness web application",
+        image:project,
+        github:"",
+        live:"https://project.alamissaoui1.repl.co/",
+    },
+    {
+        id:2,
+        name:"React JS Application",
+        image:RealEst,
+        github:"",
+        live:"",
+    },
+    {
+        id:3,
+        name:"React JS Application",
+        image:WorkIm,
+        github:"",
+        live:"",
+    },
+    {
+        id:4,
+        name:"React JS Application",
+        image:RealEst,
+        github:"",
+        live:"",
+    },
+
+
+]
